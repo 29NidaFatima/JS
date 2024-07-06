d88dab7efc67c6eb39e2acd4bb4af2bf148ebe10
NidaFatima <2529nida17@gmail.com>
2024-07-06 10:33:55 +0530

    .

--- JS-2/app.js
+++ JS-2/app.js
@@ -1,11 +1,46 @@
+//EX-1
 //Function defination
 function hello() { 
     console.log("Hello!");
 }
 //Function calling
 hello();
-
+//EX-2
 function printName() {
     console.log("Nida Fatima");
+    console.log("Anamul");
 }
 printName();
+//EX-3 Loop under function
+function print1to5() {
+for(let i=1; i<=5; i++){
+    console.log(i);
+}
+}
+print1to5();
+//EX-4 if-else under function
+function isAdult() {
+    let age=17;
+    if (age>=18) {
+        console.log("Adult");
+    } else {
+        console.log("Not Adult");
+    }
+}
+isAdult();
+
+// -----------PRACTICE QUESTION------------
+// Q1- create a function that prints a poem
+function printPoem() {
+    console.log("Twinkle Twinkle, little star");
+    console.log("how I wonder what you are");
+    console.log("Up above the world so high,");
+    console.log("Like a diamond in the sky.");
+}
+printPoem();
+// Q2- create a function to roll a dice & always display the value of dice(1to6)
+function rollDice() {
+    let value= Math.floor(Math.random()*6)+1;
+    console.log(value);
+}
+rollDice();
