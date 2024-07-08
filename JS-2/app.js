054d44064c029e7811955f0371c8af2d467bf9db
NidaFatima <2529nida17@gmail.com>
2024-07-08 12:21:32 +0530

    function

--- JS-2/app.js
+++ JS-2/app.js
@@ -133,8 +133,8 @@
 //     return result;
 // }
 // console.log(concatenation(str));
-// SCOPE
-// TYPE-1---->FUNCTION SCOPE
+//// SCOPE
+// //TYPE-1---->FUNCTION SCOPE
 // let sum=54; //global scope
 // function calSum(a,b) {
 //     let sum=a+b; //function scope
@@ -143,13 +143,13 @@
 
 // calSum(3,4);
 // console.log(sum);
-// TYPE-2---->  BLOCK SCOPE
+// // TYPE-2---->  BLOCK SCOPE
 // {
 //     let a=23;
 //     console.log(a); //block scope
 // }
 // console.log(a); // not accessible
-// TYPE-2---->  LEXICAL SCOPE
+// //TYPE-2---->  LEXICAL SCOPE
 // function outerFunc() {
 //     let x=5;
 //     let y=6;
@@ -160,7 +160,7 @@
 // }
 // outerFunc();
 
-//FUNCTION EXPRESSIONS
+// //FUNCTION EXPRESSIONS
 // let sum= function (a,b) {
 //     return a+b;
 // }
@@ -169,7 +169,7 @@
 //     console.log("helooo");
 // }
 // hello();
-//HIGHER ORDER FUNCTION
+// // HIGHER ORDER FUNCTION
 
 //Take one or multiple function as argument
 // function multipleGreet(func,count) {
@@ -184,27 +184,27 @@
 //return a function
 //  let odd=function(n){
 //     console.log(!(n%2==0));
-    
+
 //  }
 //  let even=function(n){
 //     console.log(n%2==0);
-    
+
 //  }
 // function oddOrEvenFactory(request) {
 //     if (request=="odd") {
 //         return function(n){
 //             console.log(!(n%2==0));
-            
+
 //          }  
-   
+
 //     }
-   
+
 //     if (request=="even") {
 //         return function(n){
 //             console.log(n%2==0);
-            
+
 //          }  
-        
+
 //     } 
 //     else{
 //         console.log("Wrong request");
@@ -212,7 +212,7 @@
 // }
 // let request="even";
 // console.log(oddOrEvenFactory(request));
-//METHODS
+// //METHODS
 // const calulator = {
 //     num:15,
 // add: function (a,b) {
@@ -224,23 +224,22 @@
 // mul:function (a,b) {
 //     return a*b;
 // },
+// };
+// //SHORTHAND METHOD
+// const calulator1 = {
+//     num: 15,
+//     add(a, b) {
+//         return a + b;
+//     },
+//     sub(a, b) {
+//         return a - b;
+//     },
+//     mul(a, b) {
+//         return a * b;
+//     },
 
 // };
-//SHORT HAND METHOD
-const calulator1 = {
-    num:15,
-add(a,b) {
-    return a+b;
-},
-sub(a,b) {
-    return a-b;
-},
-mul(a,b) {
-    return a*b;
-},
-
-};
-// -------------PRACTICE QUESTION------
+//  -------------PRACTICE QUESTION------
 // let greet = "hello"; //global scope
 // function changeGreet() {
 //     let greet = "Assamualaikum";
@@ -252,3 +251,86 @@ mul(a,b) {
 // }
 // console.log(greet);
 // changeGreet();
+
+//  Qs1. Write a JavaScript function that returns array elements larger than a number.
+// let arr=[2,9,10,13];
+// let num=5;
+//  function getLargerNumber(arr,num) {
+//     for(let i=0; i<=arr.length; i++){
+//         if(arr[i]>num){
+//             console.log(arr[i]);
+//         }
+//     }
+
+
+//  }
+// getLargerNumber(arr,num);
+// Qs2. Write a JavaScript function to extract unique characters from a string.
+// Example: str = “abcdabcdefgggh” ans = “abcdefgh”
+// function  uniqueCharacters(str) {
+//     let unique=''
+// for (const char of str) {
+//     if (unique.includes(char) == false) {
+//         unique+=char;
+//     }
+// }
+// return unique;
+// }
+
+//    let result=uniqueCharacters("abcdabcdefgggh");
+//    console.log(result);
+//    Qs5. Write a JavaScript function to generate a random number within a range 
+//    (start, end).
+// function generateRandomNumber() {
+//     let start=1;
+//     let end=10;
+//     let random= Math.floor(Math.random()*end)+start;
+//  console.log(random);
+    
+// }
+// generateRandomNumber();
+//ANOTHER WAY
+// let start = 100;
+// let end = 200;
+// function generateRandom(start, end) {
+// let diff = end - start;
+// return Math.floor(Math.random() * diff) + start;
+// }
+// console.log(generateRandom(start, end));
+// Qs4. Write a JavaScript function to count the number of vowels in a String 
+// argument.
+// let str="apnacollege"
+// function countVowels(str) {
+//     let count=0;
+//     for (let i = 0; i < str.length; i++) {
+//         if(str.charAt(i) == "a" ||
+//         str.charAt(i) == "e" ||
+//         str.charAt(i) == "i" ||
+//         str.charAt(i) == "o" ||
+//         str.charAt(i) == "u"){
+//             count++;
+//         }
+
+//     }
+//  return count;
+// }
+// console.log(countVowels(str));
+// Qs3. Write a JavaScript function that accepts a list of country names as input and 
+// returns the longest country name as output.
+// Example : country = ["Australia", "Germany", "United States of America"] output : 
+// "United States of America"
+let country = ["Australia", "Germany", "United States of America"]
+function longestCountryName(country) {
+    let ansIdx = 0;
+    for(let i=0; i< country.length; i++){
+        let ansLen = country[ansIdx].length;
+        let currLen = country[i].length;
+        if (currLen > ansLen) {
+        ansIdx = i;
+        
+    }
+
+}
+return ansIdx;
+}
+console.log(longestCountryName(country));
