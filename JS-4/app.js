85fd7f9c7464931daf5db14f66d79c213c10ffdb
NidaFatima <2529nida17@gmail.com>
2024-07-13 14:38:01 +0530

    Array methods

--- JS-4/app.js
+++ JS-4/app.js
@@ -116,8 +116,8 @@
 //     }
 //     });
 //     return minnumber;
-    
-      
+
+
 // }
 // console.log(getMin(nums));
 // //Default parameters
@@ -165,3 +165,86 @@
 // console.log(obj);
 // const obj1={..."NIDAFATIMA"}
 // console.log(obj1);
+//  // REST
+// function sum(...args) {
+//     for(let i=0;i<args.length;i++){
+//         console.log("you gave us:"+ args[i]);
+//     }
+// }
+// sum([1,2,3])
+// // Destructuring for array
+// let names=["nida","afsha","adiba","shabana","anamul"]
+// let[winner,runnerup,...others]=names;
+// console.log(winner,runnerup,others);
+// //Destructuring for objects
+// const student={
+//     name:"Nida",
+//     age:14,
+//     class:9,
+//     subjects:["hindi","english","math","science"],
+//     username:"29nida",
+//     password:"abcd123"
+// };
+// let{username,password}=student;
+// console.log(username,password);
+//  // PRACTICE QUESTION
+// // Qs1. Square and sum the array elements using the arrow function and then find the
+// average of the array.
+
+// let arr = [2, 4, 6, 8];
+// let square=arr.map((el)=>{
+//     return el*el;
+// })
+// console.log(square);
+
+// let sum= arr.reduce((acc,el)=>{
+//     return acc+el;
+// })
+// console.log(sum);
+
+// let average = arr.reduce((acc, el) => {
+//     return acc + el / arr.length;
+// })
+// console.log(average);
+// // Qs2. Create a new array using the map function whose each element is equal to the
+// // original element plus 5.
+// let arr = [1, 2, 3, 4, 5];
+// let newwarr=arr.map((el)=>{
+//     return el+5;
+// })
+// console.log(newwarr);
+//  //Qs3. Create a new array whose elements are in uppercase of words present in the
+// original array.
+// let array=["nida","adiba","afsha"];
+// let newArray=array.map((val)=>{
+// return val.toUpperCase();
+// })
+// console.log(newArray);
+// // Qs5. Write a function called mergeObjects that accepts two objects and returns a new
+// // object which contains all the keys and values of the first object and second object.
+// function mergeObjects(obj1, obj2) {
+//     return { ...obj1, ...obj2 };
+// }
+// let object1 = {
+//     a: "Nida",
+//     b: "Afsa"
+// };
+
+// let object2 = {
+//     c: "Niiida",
+//     d: "Afffsa"
+// };
+
+// let mergedObject = mergeObjects(object1, object2);
+// console.log(mergedObject);
+// // Qs4. Write a function called doubleAndReturnArgs which accepts an array and a
+// // variable number of arguments. The function should return a new array with the original
+// // array values and all of the additional arguments doubled.
+// const doubleAndReturnArgs=(arr,...args)=>[
+//     ...arr,
+//    args.map((el)=>
+// el*2
+//     )
+// ]
+// console.log(doubleAndReturnArgs([1,2,3],4,5,8));
+
