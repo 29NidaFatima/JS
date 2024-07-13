504fc64c6bea7dd42e6e7fe58254dbdbb4f3b6e5
NidaFatima <2529nida17@gmail.com>
2024-07-13 20:06:01 +0530

    object literals

--- JS-1/Guessing game.js
+++ JS-1/Guessing game.js
@@ -1,21 +1,21 @@
-const max =prompt("Enter the max number");
-const random=  Math.floor(Math.random()*max)+1;
+const max = prompt("Enter the max number");
+const random = Math.floor(Math.random() * max) + 1;
 console.log(random);
- let guess=prompt("Guess Number");
- while (true) {
-    if (guess=="quit") {
-      console.log("user quit");  
-      break;
+let guess = prompt("Guess Number");
+while (true) {
+    if (guess == "quit") {
+        console.log("user quit");
+        break;
     }
-    if(guess==random){
+    if (guess == random) {
         console.log("You are right! congrats random number was " + random);
         break;
     }
-    else if(guess<random){
-        guess=prompt(" HINT:Your guess was too small.please try again")
+    else if (guess < random) {
+        guess = prompt(" HINT:Your guess was too small.please try again")
     }
-    else (guess>random){
-    
-        guess=prompt(" HINT:Your guess was too large.please try again")
+    else (guess > random){
+
+        guess = prompt(" HINT:Your guess was too large.please try again")
     }
- }
+}
