9f6ef843ea66bb2c6058ba6a1bcc3148b714d296
NidaFatima <2529nida17@gmail.com>
2024-10-01 00:35:55 +0530

    callback hell &promises

--- JS-8/app.js
+++ JS-8/app.js
@@ -1,46 +1,46 @@
 // Call Stack
 function hello() {
     console.log("inside hello function");
-    
+
     console.log("Hello!");
-    
+
 }
-function demo(){
+function demo() {
     console.log("calling hello function");
-    
-   hello();
-    
+
+    hello();
+
 }
 console.log("calling demo function");
 
 demo();
 console.log("Done Bye!");
- // Visualizing the Call Stack
-function one(){
+// Visualizing the Call Stack
+function one() {
     return 1;
 }
-function two(){
-    return one()+ one();
+function two() {
+    return one() + one();
 }
-function three(){
-     let ans= two() + one();
-     console.log(ans);
-     
+function three() {
+    let ans = two() + one();
+    console.log(ans);
+
 }
 three();
 // JS is Single Threaded Language
-let a=29;
+let a = 29;
 console.log(a);
-let b=10;
+let b = 10;
 console.log(b);
-console.log(a+b);
+console.log(a + b);
 
-setTimeout(()=>{
+setTimeout(() => {
     console.log("Apna College");
-    
-},2000)
-setTimeout(()=>{
+
+}, 2000)
+setTimeout(() => {
     console.log("Hello World");
-    
-},2000)
+
+}, 2000)
 console.log("Hello.......");
--- JS-8/app1.js
+++ JS-8/app1.js
@@ -1,65 +1,65 @@
 // CallbackHell
 let h1 = document.querySelector("h1");
-function changeColor(color,delay,nextColorChange) {
-    setTimeout(()=>{
+function changeColor(color, delay, nextColorChange) {
+    setTimeout(() => {
         h1.style.color = color;
         nextColorChange();
-    },delay)
+    }, delay)
 
 }
 // ----------------------------------
-setTimeout(()=>{
+setTimeout(() => {
     changeColor("red");
 }, 1000);
 
-setTimeout(()=> {
+setTimeout(() => {
     changeColor("orange");
 }, 2000);
 
-setTimeout(()=>{
+setTimeout(() => {
     changeColor("green");
 }, 3000);
 
 //Better way tor write this above code
 //callbacks nesting-->Callback Hell
-changeColor("red",1000,()=>{
-    changeColor("orange",1000,()=>{
-        changeColor("green",1000,()=>{
-            changeColor("yellow",1000)
+changeColor("red", 1000, () => {
+    changeColor("orange", 1000, () => {
+        changeColor("green", 1000, () => {
+            changeColor("yellow", 1000)
         });
     });
 });
 // Promises
-function saveToDb(data,succes,failure){
-     let internetSpeed=Math.floor(Math.random()*10)+1;
-    if (internetSpeed>4) {
-       succes();   
+function saveToDb(data, succes, failure) {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 4) {
+        succes();
     }
-    else{
-       failure();
+    else {
+        failure();
     }
 
 
 }
 // fn call
-saveToDb("apna college",()=>{
+saveToDb("apna college", () => {
     console.log(" success1:your data was saved ");
-    saveToDb("hello World",()=>{
+    saveToDb("hello World", () => {
         console.log("success2:your data was saved ");
-        saveToDb("Nida",()=>{
-            console.log("success3:your data was saved "); 
-        },()=>{
+        saveToDb("Nida", () => {
+            console.log("success3:your data was saved ");
+        }, () => {
             console.log("failure2:weak connection.Data not saved");
         })
-    },()=>{
+    }, () => {
         console.log("failure2:weak connection.Data not saved");
 
     })
-},()=>{
+}, () => {
     console.log(" failure1:weak connection.Data not saved");
 
 })
- // ----------------------Promises working---------------------
+// ----------------------Promises working---------------------
 function saveToDb(data) {
 
     return new Promise((resolve, reject) => {
@@ -90,87 +90,87 @@ request.then(() => {
         console.log(request);
 
     })
- // Compact way of writing above code
+// Compact way of writing above code
 saveToDb("apna college")
-.then(() => {
-    console.log("resolved");
-})
+    .then(() => {
+        console.log("resolved");
+    })
     .catch(() => {
         console.log("rejected");
-    
+
 
     })
-    //  Promise Chaining
-    saveToDb("apna college")
-.then(() => {
-    console.log(" DATA:1 Saved,resolved");
-    saveToDb("hello world")
-    .then(()=>{
-        console.log("DATA:2 saved,resolved");
-        
-    })
+//  Promise Chaining
+saveToDb("apna college")
+    .then(() => {
+        console.log(" DATA:1 Saved,resolved");
+        saveToDb("hello world")
+            .then(() => {
+                console.log("DATA:2 saved,resolved");
 
-})
+            })
+
+    })
     .catch(() => {
         console.log("rejected");
-    
+
 
     })
-    // Improved version to write this code(more readable)
-    saveToDb("apna college")
+// Improved version to write this code(more readable)
+saveToDb("apna college")
     .then((result) => {
-        
+
         console.log(" DATA:1 Saved,resolved");
-        console.log("result of promise:",result);
-        
-         return saveToDb("hello world")
-       
-    
+        console.log("result of promise:", result);
+
+        return saveToDb("hello world")
+
+
     })
-    .then((result)=>{
+    .then((result) => {
         console.log(" DATA:1 Saved,resolved");
-        console.log("result of promise:",result);
+        console.log("result of promise:", result);
         return saveToDb("Nida")
     })
-    .then((result)=>{
+    .then((result) => {
         console.log(" DATA:3 Saved,resolved");
-        console.log("result of promise:",result);
-        
+        console.log("result of promise:", result);
+
     })
-        .catch((error) => {
-            console.log("rejected");
-            console.log("error of promise:",error);
-            
-        
-    
-        })
-    // Let's apply promises on callback hell
-    let h1 = document.querySelector("h1");
-function changeColor(color,delay) {
-     return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
+    .catch((error) => {
+        console.log("rejected");
+        console.log("error of promise:", error);
+
+
+
+    })
+// Let's apply promises on callback hell
+let h1 = document.querySelector("h1");
+function changeColor(color, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
             h1.style.color = color;
             resolve("color changed")
-        },delay)
+        }, delay)
     })
-    
+
 
 }
 // -------fn call------
-changeColor("red",1000)
-.then(()=>{
-    console.log("red color was completed");
-    return changeColor("orange",1000)
-})
-.then(()=>{
-    console.log("orange color was completed");
-    return changeColor("green",1000)
-})
-.then(()=>{
-    console.log("green color was completed");
-    return changeColor("yellow",1000)
-})
-.then(()=>{
-    console.log("yellow color was completed");
-  
-})
+changeColor("red", 1000)
+    .then(() => {
+        console.log("red color was completed");
+        return changeColor("orange", 1000)
+    })
+    .then(() => {
+        console.log("orange color was completed");
+        return changeColor("green", 1000)
+    })
+    .then(() => {
+        console.log("green color was completed");
+        return changeColor("yellow", 1000)
+    })
+    .then(() => {
+        console.log("yellow color was completed");
+
+    })
